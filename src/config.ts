e601bd786dbb639b7b6f9f5f0854250d0ebdd070
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2022-09-25 22:40:31 -0400

    improving index page

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Manuel Ernesto Garcia';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Software Engineer from Cuba and Entrepreneur. Working in create software that solve problems in a simple way and sharing what I learn along the way.';
