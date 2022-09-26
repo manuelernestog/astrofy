9cb339941cc37362cb8ce92c5797492553c2acf4
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2022-09-26 15:37:08 -0400

    adding details to template

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Manuel Ernesto Garcia';
-export const SITE_DESCRIPTION = 'Software Engineer from Cuba and Entrepreneur. Working in create software that solve problems in a simple way and sharing what I learn along the way.';
+export const SITE_TITLE = 'Astro Personal Website Template';
+export const SITE_DESCRIPTION = 'Astro personal website template';
