ad81467c968f46911e9c69e96985dc3ee480044e
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2022-09-27 11:50:52 -0400

    updating template tags

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Personal Website Template';
-export const SITE_DESCRIPTION = 'Astro personal website template';
+export const SITE_TITLE = 'Astro Minimalist Personal Website Template';
+export const SITE_DESCRIPTION = 'Astro Minimalist Personal Website template with Project Section, CV Section, Paginated Blog, RSS Feed, SEO Friendly, Visual themes and Responsive Desing.';
