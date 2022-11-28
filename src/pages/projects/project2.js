aaa93a5dee53f4608e0ea42c941ead5fb9a17362
Rafael Planas <69021807+mybess00@users.noreply.github.com>
2022-11-28 13:54:19 -0500

    update all

---
+++ src/pages/projects/project2.js
@@ -0,0 +1,6 @@
+{
+    title="eManager"
+    img="/post_img_emanager.webp"
+    desc="Es una aplicación móvil desarrollada con el objetivo de administrar los servicios de Etecsa de la manera más minimalista y práctica posible. Cuenta con todas las herramientas necesarias para hacer un uso rápido e intuitivo de los servicios de Etecsa, tales como consulta de saldo, planes combinados, paquetes de datos, voz y mensajes. eManager organiza tus últimas operaciones proporcionadas por Transfermóvil, te permite autenticarte y consulta tus últimas operaciones sin salir de la aplicación. Puedes usar la función de eTransfer para transferir y recibir saldo mediante códigos QR."
+    url="https://www.apklis.cu/application/com.raise.studios.emanager"
+}
