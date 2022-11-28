fd71e852c65f19e4f1acdaa91f9b50d4703a981c
Rafael Planas <69021807+mybess00@users.noreply.github.com>
2022-11-28 08:18:18 -0500

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-modern-personal-website.netlify.app',
+  site: 'https://astro-modern-personal-website.vercel.app/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
