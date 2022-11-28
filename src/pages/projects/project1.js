b1ce22da936b0b6c2864419ea1a1a5a4cc6e06e3
Rafael Planas <69021807+mybess00@users.noreply.github.com>
2022-11-28 14:09:45 -0500

    update all 3

--- src/pages/projects/project1.js
+++
@@ -1,7 +0,0 @@
-{
-    title="BitNautas"
-    img="/post_img_bitnautas.webp"
-    desc="Es una comunidad cubana relacionada con las criptomonedas, NFT, Web3, blockchain y mucho más. Los miembros  tienen acceso sin costo y en todo momento a el contenido educativo publicado en la comunidad."
-    url="https://bitnautas_cu.point.link/"
-    badge="TOP"
-}
