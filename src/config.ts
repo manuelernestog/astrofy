0b8a88b0a0d2579cd51c2a8eb78d028b276f8a03
Rafael Planas <69021807+mybess00@users.noreply.github.com>
2022-11-28 10:18:58 -0500

    update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Rafael PB Personal Website';
+export const SITE_TITLE = 'Rafael Planas Personal Website';
 export const SITE_DESCRIPTION = 'Información personal y pública sobre Rafael Planas. Te cuento mis habilidades, experiencias y proyectos en los que trabajo.';
