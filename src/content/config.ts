835999cf1bfdc31211bcec5f769f1e41403875c6
carloszbent <bentsoft365@gmail.com>
2023-03-12 15:56:56 -0400

    feat: added badges to Blog entries

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,6 +6,7 @@ const blogSchema = z.object({
     pubDate: z.coerce.date(),
     updatedDate: z.string().optional(),
     heroImage: z.string().optional(),
+    badge: z.string().optional(),
 });
 
 const storeSchema = z.object({
