d90ca1aa6c4841174b46bb226eabe5f797b40c11
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-03-21 15:24:26 -0400

    getting data from collection for rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,18 @@
-import rss from '@astrojs/rss';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
+import rss from "@astrojs/rss";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
+import { getCollection } from "astro:content";
 
-export const get = () =>
-	rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.md'),
-	});
+export async function get(context) {
+  const blog = await getCollection("blog");
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: import.meta.env.SITE,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blog/${post.slug}/`,
+    })),
+  });
+}
