99e0a60224b1b00af3f4991b53c406f60e2a5c8f
brahimABD98 <brahim.abderrahim@esprit.tn>
2023-04-10 13:59:08 +0100

    added astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-modern-personal-website.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind(), image(
+    {
+      serviceEntryPoint: '@astrojs/image/sharp',
+      cacheDir: "./.cache/image",
+      logLevel: 'debug',
+    }
+  )]
 });
