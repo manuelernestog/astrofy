d9fd08658c5b6f94bdda16f9aeaf4f6a56f67076
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-06-07 14:05:24 -0400

    changing name to astrofy

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Modern Personal Website Template';
-export const SITE_DESCRIPTION = 'Astro Modern Personal Website template with Project Section, CV Section, Paginated Blog, RSS Feed, SEO Friendly, Visual themes and Responsive Desing.';
+export const SITE_TITLE = 'Astrofy | Astro Personal Portfolio Website';
+export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website.';
