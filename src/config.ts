c2d62920ced20bd948e389f93a2c59d05e0cf41e
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-06-07 14:26:06 -0400

    updating name and description

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Astro Personal Portfolio Website';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website.';
+export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
+export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes your website with a Project Section, CV, Store, Blog, RSS Feed and 29 Visual themes.';
