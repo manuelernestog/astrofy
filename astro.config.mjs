757075aa030ce49356bd71d268163bfa2a11f247
ahmadikhwan <ahmadikhwan258@gmail.com>
2023-06-30 15:46:32 +0800

    deploy config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://ahmadikhwan.github.io',
   integrations: [mdx(), sitemap(), tailwind(), image(
     {
       serviceEntryPoint: '@astrojs/image/sharp',
