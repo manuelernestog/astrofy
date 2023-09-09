8dcd5258701e4db986658cb2f968b38032a5bb4d
brahimABD98 <brahim.abderrahim@esprit.tn>
2023-09-09 02:01:44 +0100

    added tags

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,4 @@
 import { z, defineCollection } from "astro:content";
-
 const blogSchema = z.object({
     title: z.string(),
     description: z.string(),
@@ -7,6 +6,9 @@ const blogSchema = z.object({
     updatedDate: z.string().optional(),
     heroImage: z.string().optional(),
     badge: z.string().optional(),
+    tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
+        message: 'tags must be unique',
+    }).optional(),
 });
 
 const storeSchema = z.object({
@@ -16,7 +18,7 @@ const storeSchema = z.object({
     custom_link: z.string().optional(),
     updatedDate: z.coerce.date(),
     pricing: z.string().optional(),
-    oldPricing:  z.string().optional(),
+    oldPricing: z.string().optional(),
     badge: z.string().optional(),
     checkoutUrl: z.string().optional(),
     heroImage: z.string().optional(),
