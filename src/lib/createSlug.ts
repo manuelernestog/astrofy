61ebae54df9cdcc98b65bd239f0be39dc77cd484
Jordi Ayala <jord7as@gmail.com>
2024-01-29 12:11:44 -0600

    fix to include accents in blog slug

--- src/lib/createSlug.ts
+++ src/lib/createSlug.ts
@@ -3,17 +3,28 @@
 import { GENERATE_SLUG_FROM_TITLE } from '../config'
 
 export default function (title: string, staticSlug: string) {
+  const accentsMap = {
+      'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u',
+      'à': 'a', 'è': 'e', 'ì': 'i', 'ò': 'o', 'ù': 'u',
+      'ä': 'a', 'ë': 'e', 'ï': 'i', 'ö': 'o', 'ü': 'u',
+      'â': 'a', 'ê': 'e', 'î': 'i', 'ô': 'o', 'û': 'u'
+  };
+
   return (
-    !GENERATE_SLUG_FROM_TITLE ? staticSlug : title
-      // remove leading & trailing whitespace
-      .trim()
-      // output lowercase
-      .toLowerCase()
-      // replace spaces
-      .replace(/\s+/g, '-')
-      // remove special characters
-      .replace(/[^\w-]/g, '')
-      // remove leading & trailing separtors
-      .replace(/^-+|-+$/g, '')
+      !GENERATE_SLUG_FROM_TITLE ? staticSlug : title
+          // remove leading & trailing whitespace
+          .trim()
+          // replace accented characters
+          .replace(/[\u00C0-\u00FF]/g, function(char) {
+              return accentsMap[char] || char;
+          })
+          // output lowercase
+          .toLowerCase()
+          // replace spaces
+          .replace(/\s+/g, '-')
+          // remove special characters
+          .replace(/[^\w-]/g, '')
+          // remove leading & trailing separators
+          .replace(/^-+|-+$/g, '')
   )
 }
