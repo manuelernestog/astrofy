27d84e39716493fc1748bd154e95d92f9c946f6b
fzhg <m42zhang@uwaterloo.ca>
2024-03-11 23:48:49 -0400

    fixed issue #67

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,9 +2,10 @@ import rss from "@astrojs/rss";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import { getCollection } from "astro:content";
 
-export async function get(context) {
+export async function GET(context) {
   const blog = await getCollection("blog");
   return rss({
+    stylesheet: "/rss/styles.xsl",
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
